d0694d516505b7a2c6dcf819a9a03a1da0e9774f
skwiera12 <80981969+skwiera12@users.noreply.github.com>
2022-05-02 18:31:09 +0200

    Create configurek8s-workers.sh

---
+++ configurek8s-workers.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## add private ips for master and worker nodes
+sudo sed -i '/^127.0.0.1.*/a 10.0.0.5 k8smaster\n10.0.0.6 k8snode1\n10.0.0.7 k8snode2' /etc/hosts
+
+## on all nodes setup containerd
+cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+
+## install and configure containerd
+sudo apt-get update && sudo apt-get install -y containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+## on all nodes disable swap
+sudo swapoff -a
+
+## on all noed install kubeadm,kubelet and kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet=1.23.0-00 kubeadm=1.23.0-00 kubectl=1.23.0-00
+sudo apt-mark hold kubelet kubeadm kubectl
+
+## Get token to join workers to cluster
+## kubeadm token create --print-join-command
+
+## On workers join them using token
+## sudo kubeadm join "token"
